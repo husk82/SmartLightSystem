8078f82237253c9988451ce207d9096e43693d60
Aashish Tako <takoaashish01@gmail.com>
2025-10-28 14:33:51 -0500

    Fixed minor bugs

--- flask_server/database/setup_db.py
+++ flask_server/database/setup_db.py
@@ -6,16 +6,16 @@ def create_table():
         conn = psycopg2.connect(**DB_CONFIG)
         with conn.cursor() as cur:
             cur.execute("""
-                CREATE TABLE IF NOT EXIST lux_readings (
+                CREATE TABLE IF NOT EXISTS lux_readings (
                     id SERIAL PRIMARY KEY,
                     lux_value FLOAT NOT NULL,
-                    timestamp TIMESTAMP NOT NULL
+                    timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
                 )
                 """)
             conn.commit()
         print ("Table 'lux_readings' created successfully")
     except Exception as e:
-        print("Error creating table: {e}")
+        print(f"Error creating table: {e}")
     finally:
         if conn:
               conn.close()
